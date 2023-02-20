a74b742c9141acfd58c65fbcbb690bfc77fc604b
rmoghariya7 <rmoghariya7@gmail.com>
2023-02-20 17:14:30 +0530

    increase the win factor

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ btnHold.addEventListener("click", function () {
   document.querySelector(`#score--${activePlayer}`).textContent =
     scores[activePlayer];
 
-  if (scores[activePlayer] >= 20) {
+  if (scores[activePlayer] >= 50) {
     document
       .querySelector(`.player--${activePlayer}`)
       .classList.add("player--winner");
